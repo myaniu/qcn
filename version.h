26f3da3588872aa5794e5ccaa47d0aef43fced5c
Carl Christensen <carlgt1@yahoo.com>
2009-06-27 23:32:06 +0000

    vn

--- version.h
+++ version.h
@@ -7,12 +7,12 @@
 #define QCN_MAJOR_VERSION 5
 
 /* Minor part of QCN version number */
-#define QCN_MINOR_VERSION 6
+#define QCN_MINOR_VERSION 8
 
 /* Release part of QCN version number */
 #define QCN_RELEASE 
 
 /* String representation of QCN version number */
-#define QCN_VERSION_STRING "5.06"
+#define QCN_VERSION_STRING "5.08"
 
 #endif /* #ifndef QCN_VERSION_H */
