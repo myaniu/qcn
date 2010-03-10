1ee745a7d19f42a486d6ae7d29ed272a5395dd19
Carl Christensen <carlgt1@yahoo.com>
2010-03-10 03:22:44 +0000

    M    version.h

--- version.h
+++ version.h
@@ -7,12 +7,12 @@
 #define QCN_MAJOR_VERSION 5
 
 /* Minor part of QCN version number */
-#define QCN_MINOR_VERSION 52
+#define QCN_MINOR_VERSION 54
 
 /* Release part of QCN version number */
 #define QCN_RELEASE 
 
 /* String representation of QCN version number */
-#define QCN_VERSION_STRING "5.52"
+#define QCN_VERSION_STRING "5.54"
 
 #endif /* #ifndef QCN_VERSION_H */
