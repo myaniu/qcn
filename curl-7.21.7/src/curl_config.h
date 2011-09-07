786f93c8d137fb5f72075958e9974b3e4293a2ef
Carl Christensen <carlgt1@yahoo.com>
2011-09-07 03:43:33 +0000

    rebuilt on Mac OS X Lion 10.6

--- curl-7.21.7/src/curl_config.h
+++ curl-7.21.7/src/curl_config.h
@@ -815,7 +815,7 @@
 /* #undef NEED_THREAD_SAFE */
 
 /* cpu-machine-OS */
-#define OS "x86_64-apple-darwin11.0.1"
+#define OS "x86_64-apple-darwin11.1.0"
 
 /* Name of package */
 #define PACKAGE "curl"
