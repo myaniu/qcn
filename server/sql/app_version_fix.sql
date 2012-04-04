de5470466282900d1d061474e0046190252b1186
Carl Christensen <carlgt1@yahoo.com>
2012-04-04 14:54:48 +0000

    M    server/sql/app_version_fix.sql

--- server/sql/app_version_fix.sql
+++ server/sql/app_version_fix.sql
@@ -2,14 +2,10 @@
 
 use sensor;
 
-update app_version set xml_doc=replace(xml_doc, '<version_num>681</version_name>', '<version_num>681</version_num>') where version_num=681;
-
-update app_version set xml_doc=replace(xml_doc, '<version_num>680</version_name>', '<version_num>680</version_num>') where version_num=680;
+update app_version set xml_doc=replace(xml_doc, '</version_name>', '</version_num>');
 
 use continual;
-update app_version set xml_doc=replace(xml_doc, '<version_num>681</version_name>', '<version_num>681</version_num>') where version_num=681;
 
-update app_version set xml_doc=replace(xml_doc, '<version_num>680</version_name>', '<version_num>680</version_num>') where version_num=680;
-
+update app_version set xml_doc=replace(xml_doc, '</version_name>', '</version_num>');
 
 
