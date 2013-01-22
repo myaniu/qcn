2bf396f0bf4dd0c2acd6fd06e4834bd2c0f63457
Carl Christensen <carlgt1@yahoo.com>
2013-01-22 08:20:53 -0800

    but in prefs_util

--- server/html/user/earthquake_email.php
+++ server/html/user/earthquake_email.php
@@ -1,15 +1,15 @@
 <?php
 chdir("/var/www/boinc/sensor/html/user");
 require_once("/var/www/boinc/sensor/html/inc/earthquake_email.inc");
-$mag  = 3.3; 
-$longitude = 172.503; 
-$latitude = -43.595; 
-$depth = 9.4; 
-$n_stations = 7; 
-$etime = 1355979285.391479; 
-$dtime = 1355979300; 
-$dt_detect  = 14.6; 
-$edir       = 1355979285; 
+$mag  = 3.5; 
+$longitude = 172.455; 
+$latitude = -43.587; 
+$depth = 5.9; 
+$n_stations = 9; 
+$etime = 1358583310.045698; 
+$dtime = 1358583319; 
+$dt_detect  = 9.0; 
+$edir       = 1358583310; 
 
 earthquake_email($mag,$longitude,$latitude,$depth,$n_stations,$etime,$edir,$dtime,$dt_detect);
 
